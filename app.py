2781da0c00500fdedf8b378801002961c7069d0f
Pierre Liebenberg <pierrelieb@gmail.com>
2022-09-24 17:21:59 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import json
+import streamlit as st
+import numpy 
+
+from ee_utils import *
+
+# Home view
+# Pie chart of holdings, industries etc.
+# Risk score???
+
+# Specific holdings views 
+
+#
+
+
+
